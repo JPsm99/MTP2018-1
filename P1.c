a85ff7dae7c5fe24ff442f733fb70ded165a83b6
João Pedro <37358850+JPsm99@users.noreply.github.com>
2018-07-10 20:50:08 -0300

    Update P1.c

--- P1.c
+++ P1.c
@@ -1,74 +1,47 @@
-//Jo„o Pedro Silva Milagre
+//Jo√£o Pedro Silva Milagre
 //11721EEL003
 
 #include <stdio.h>
-
 int main()
 {
-    int estado, x=0, r=0;
-    char bits[256];
-    do{
-        printf("Insira uma sequencia binaria: ");
-        scanf("%s", &bits);
-        while (bits[x]!='\0')
-        {
-            if(bits[x]!='0' && bits[x]!='1')
-                r++;
-            x++;
-        }
-        if(r!=0)
-            printf("\nO numero nao e binario. Insira novamente.\n");
-    }
-    while(r!=0);
-	if (bits[0]=='0'){
-        estado=0;
-        x=1;
-        while(bits[x]!='\0'){
-            if(estado==0 && bits[x]=='0')
-                estado=0;
-            else if(estado==0 && bits[x]=='1')
-                estado=1;
-            else if(estado==1 && bits[x]=='0')
-                estado=2;
-            else if(estado==1 && bits[x]=='1')
-                estado=0;
-            else if(estado==2 && bits[x]=='0')
-                estado=1;
-            else if(estado==2 && bits[x]=='1')
-                estado=2;
-            x++;
-        }
-        printf("\nO estado de aceitacao final e: %d", estado);
-		printf("\nI)O numero digitado foi: %s", bits);
-        if (estado==0)
-            printf("\nII)A sequencia binaria inserida e multipla de 3.");
-        if (estado!=0)
-            printf("\nII)A sequencia binaria inserida nao e multipla de 3.");
-    }
-    if(bits[0]=='1'){
-        estado=1;
-        x=1;
-        while(bits[x]!='\0'){
-            if (estado==0 && bits[x]=='0')
-                estado=0;
-            else if(estado==0 && bits[x]=='1')
-                estado=1;
-            else if(estado==1 && bits[x]=='0')
-                estado=2;
-            else if(estado==1 && bits[x]=='1')
-                estado=0;
-            else if(estado==2 && bits[x]=='0')
-                estado=1;
-            else if(estado==2 && bits[x]=='1')
-                estado=2;
-            x++;
-        }
-        printf("\nO estado de aceitacao final e: %d", estado);
-		printf("\nI)O numero digitado foi: %s", bits);
-        if (estado==0)
-            printf("\nII)A sequencia binaria inserida e multipla de 3!");
-        if (estado!=0)
-            printf("\nII)A sequencia binaria inserida nao e multipla de 3.");
-    }
-    return 0;
+	int est=0, i=0, j=0;
+	char bits[256];
+	while (j>=0)
+	{
+		j=0;
+		printf("Insira um numero em binario para ver se este e multiplo 3: ");
+	    scanf("%s", bits);
+	    for (i=0; bits[i]!='\0'; i++)
+	    {
+	    	if (bits[i]!='0' && bits[i]!='1')
+	    	j++;
+		}
+		j--;
+	}
+	for (i=0; bits[i]!='\0'; i++)
+	{
+		if (bits[i]=='0')
+		{
+			if (est==0)
+				est=0;
+			else if (est==2)
+			    est=1;
+			else
+			    est=2;
+		}
+		else if (bits[i]=='1')
+		{
+			if (est==2)
+			    est=2;
+			else if (est==0)
+			    est=1;
+			else
+			    est=0;
+		}
+	}
+	if (est==0)
+	    printf("\n%s e um multiplo de 3", bits);
+	else
+	    printf("\n%s nao um multiplo de 3", bits);
+	return 0;
 }
